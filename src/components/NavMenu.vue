8fcea3141df8bbd5ffac62c7583497d5ced19e51
ivanttoprogram <123283030+ivanttoprogram@users.noreply.github.com>
2024-04-07 22:25:14 -0500

    Initial commit

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="bg-slate-800 h-20 items-center text-white flex justify-center space-x-6">
+    <RouterLink :to="{name: 'Home'}">Home</RouterLink>
+    <RouterLink :to="{name: 'Other'}">Other</RouterLink>
+  
+    <RouterLink :to="{name: 'SettingsPage'}">Settings</RouterLink>
+
+    <div v-if="isAuthenticated">
+    Welcome {{  user.email }}
+    <button @click="logout" class="rounded-md bg-red-600 px-4 py-2 mx-4 text-red-200 hover:bg-red-800">LOGOUT</button>
+    </div>
+    <div v-else>
+        <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
+    </div>
+  </nav>
+</template>
+
+<!--<style lang="postcss" scoped>
+nav {
+  @apply flex justify-center space-x-4;
+
+  & .router-link-active {
+    @apply underline underline-offset-4;
+  }
+}
+</style> -->
+
+<script setup>
+import { useAuth } from '../composables/useAuth'
+const { isAuthenticated, logout, user } = useAuth()
+
+</script>
---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGx7R4tp0MCl8jztcuCDDuGTOsgFDDJ3Q",
+  authDomain: "company-directory-84b9d.firebaseapp.com",
+  projectId: "company-directory-84b9d",
+  storageBucket: "company-directory-84b9d.appspot.com",
+  messagingSenderId: "674849351750",
+  appId: "1:674849351750:web:c0dfbe53d4875908868247"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
