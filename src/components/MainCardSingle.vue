32d35dcf65a4116b629c1ba86ba92015245c9a6d
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2024-04-08 02:05:55 -0500

    connect to API

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,12 +1,32 @@
 <script setup>
 import { faker } from '@faker-js/faker'
 
-const firstname = faker.person.firstName()
-const lastname = faker.person.lastName()
-const fullname = faker.person.fullName({firstName: firstname, lastName:lastname})
-const job_title = faker.person.jobTitle()
-const bio = faker.lorem.lines(2)
-const username = faker.internet.userName({firstName: firstname, lastName:lastname}).toLowerCase()
+// const firstname = faker.person.firstName()
+// const lastname = faker.person.lastName()
+// const fullname = faker.person.fullName({firstName: firstname, lastName:lastname})
+// const job_title = faker.person.jobTitle()
+// const bio = faker.lorem.lines(2)
+// const username = faker.internet.userName({firstName: firstname, lastName:lastname}).toLowerCase()
+
+const props = defineProps({
+  employee: {
+    type: Object, 
+    required: true, 
+    default: () => {
+      return {
+        "_id": "65f1b471c1d4a06a0786c3bc",
+        "userName": "jperez",
+        "firstName": "Joseph",
+        "lastName": "Perez",
+        "title": "Computer Science Wizard",
+        "_V": 0
+
+      }
+    },
+  },
+})
+
+
 </script>
 
 <template>
@@ -15,9 +35,9 @@ const username = faker.internet.userName({firstName: firstname, lastName:lastnam
   <div class="border border-blue-900 rounded-lg shadow bg-blue-400 h-full">
     <img :src="faker.image.urlLoremFlickr({category: 'people'})" class="rounded-t-xl"/>
     <div class="p-4">
-      <p class="mb-2 text-3xl font-bold">{{ fullname }}</p>
-      <p class="mb-3 font-normal text-black">{{ job_title }}</p>
-      <p class="mb-3 font-normal text-black">{{ bio }}</p>
+      <p class="mb-2 text-3xl font-bold">{{ props.employee.firstName }} {{ props.employee.lastName }}</p>
+      <p class="mb-3 font-normal text-black">{{ props.employee.title }}</p>1
+      <p class="mb-3 font-normal text-black">{{ props.employee.quote }}</p>
     </div>
   </div>
   </RouterLink>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,8 +3,22 @@
 //const title = ref('Hello There ✋')
 import MainCards from '@/components/MainCards.vue';
 import MainPagination from '@/components/MainPagination.vue';
+import useAPI from '@/composables/useAPI';
+
+const { getEmployees, loading } = useAPI()
+
+onMounted(async () => {
+  await getEmployees()
+})
+
 </script>
 
 <template>
+
+  <p v-id="loading">LOADING...</p>
+  <div v-else>
   <MainCards></MainCards>
+  <MainPagination></MainPagination>
+  </div>
+  
 </template>
